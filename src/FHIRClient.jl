a7085579b799593bb715d032a2e486a8ac85467f
Dilum Aluthge <dilum@aluthge.com>
2020-07-30 15:21:24 -0400

    Add the `FHIRType` abstract type (#4)

--- src/FHIRClient.jl
+++ src/FHIRClient.jl
@@ -4,4 +4,11 @@ import HTTP
 import JSON3
 # import OMOPCommonDataModel
 
+export FHIRType
+
+"""
+A `FHIRType` is a Julia type representation of a FHIR object.
+"""
+abstract type FHIRType end
+
 end # end module FHIRClient
