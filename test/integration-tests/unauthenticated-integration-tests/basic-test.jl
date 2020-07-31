f45a991376c4bf57b96d1e29cb71a09b9e9a893b
Dilum Aluthge <dilum@aluthge.com>
2020-07-31 06:08:58 -0400

    Add support for sending authentication in HTTP headers

--- test/integration-tests/unauthenticated-integration-tests/basic-test.jl
+++ test/integration-tests/unauthenticated-integration-tests/basic-test.jl
@@ -3,17 +3,25 @@ using FHIRClient
 using Test
 
 @testset "Basic test" begin
-    endpoint = FHIRClient.Endpoint("https://hapi.fhir.org/baseR4")
-    fhir_version = FHIRClient.R4()
-    client = FHIRClient.Client(fhir_version, endpoint)
-    @test FHIRClient.get_fhir_version(client) == fhir_version
-    @test FHIRClient.get_endpoint(client) == endpoint
-    query = "/Patient/22692"
-    response = FHIRClient.fhir_get_json(client, query)
-    @test response.resourceType == "Patient"
-    @test response.id == "22692"
-    @test length(response.name) == 1
-    @test response.name[1].text == "Jason Argonaut"
-    @test response.name[1].family == "Argonaut"
-    @test response.name[1].given == String["Jason"]
+    all_auths = [
+        FHIRClient.AnonymousAuth(),
+        FHIRClient.OAuth2("foobar"),
+        FHIRClient.JWTAuth("foobar"),
+        FHIRClient.UsernamePassAuth("helloworld", "foobar"),
+    ]
+    for auth in all_auths
+        endpoint = FHIRClient.Endpoint("https://hapi.fhir.org/baseR4")
+        fhir_version = FHIRClient.R4()
+        client = FHIRClient.Client(fhir_version, endpoint, auth)
+        @test FHIRClient.get_fhir_version(client) == fhir_version
+        @test FHIRClient.get_endpoint(client) == endpoint
+        query = "/Patient/22692"
+        response = FHIRClient.fhir_get_json(client, query)
+        @test response.resourceType == "Patient"
+        @test response.id == "22692"
+        @test length(response.name) == 1
+        @test response.name[1].text == "Jason Argonaut"
+        @test response.name[1].family == "Argonaut"
+        @test response.name[1].given == String["Jason"]
+    end
 end
