d56013421bc2cc4dcb146f42ec7799c674c07384
Dilum Aluthge <dilum@aluthge.com>
2020-08-02 05:00:53 -0400

    Fix a bug in the test suite (#27)

--- test/test-mutable-struct-equality.jl
+++ test/test-mutable-struct-equality.jl
@@ -5,7 +5,7 @@ function _mutable_struct_equality(a::T, b::T)::Bool where T
         temp_vector = Vector{Bool}(undef, T_num_fieldnames)
         for i in 1:T_num_fieldnames
             name_i = T_fieldnames[i]
-            temp_vector[i] = getfield(a, name_i) == getfield(b, name_i)
+            temp_vector[i] = _mutable_struct_equality(getfield(a, name_i), getfield(b, name_i))
         end
         return all(temp_vector)
     else
