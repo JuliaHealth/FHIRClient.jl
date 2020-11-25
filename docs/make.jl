28dea784997d8fffe01a8a6c470c7ab2667e56d5
Dilum Aluthge <dilum@aluthge.com>
2020-11-25 15:25:10 -0500

    Add installation instructions

--- docs/make.jl
+++ docs/make.jl
@@ -14,6 +14,7 @@ makedocs(;
     ),
     pages=[
         "Home" => "index.md",
+        "Installing FHIRClient" => "installation.md",
         "Examples" => "examples.md",
         "API" => "api.md",
         "Auto-generating the type definitions" => "generate.md",
---
+++ docs/src/installation.md
@@ -0,0 +1,12 @@
+```@meta
+CurrentModule = FHIRClient
+```
+
+# Installing FHIRClient
+
+To install FHIRClient, open Julia and run the following commands:
+```julia
+julia> import Pkg
+
+julia> Pkg.add("FHIRClient")
+```
