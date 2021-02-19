4e9b6555f3623c58712bcfb5361e40b745769ef3
Dilum Aluthge <dilum@aluthge.com>
2021-02-19 10:57:37 -0500

    Fix CI

--- .ci/generate/develop.jl
+++ .ci/generate/develop.jl
@@ -3,7 +3,7 @@ import Pkg
 Pkg.activate(@__DIR__)
 
 devpkgs = [
-    PackageSpec(path = joinpath(dirname(dirname(@__DIR__)), "Generate")),
+    Pkg.PackageSpec(path = joinpath(dirname(dirname(@__DIR__)), "Generate")),
 ]
 
 Pkg.develop(devpkgs)
