300647ad1155edfac8f375d12950ab391cbe639d
Dilum Aluthge <dilum@aluthge.com>
2023-02-27 21:26:42 -0500

    Fix the tests

--- test/unit/types.jl
+++ test/unit/types.jl
@@ -1,13 +1,13 @@
 @testset "BaseURL constructor" begin
     https_uris = ["https://example.com", HTTP.URI("https://example.com")]
-    @testset for uri in uris
+    @testset for uri in https_uris
         @test FHIRClient.BaseURL(uri) isa FHIRClient.BaseURL
         @test FHIRClient.BaseURL(uri; require_https = true) isa FHIRClient.BaseURL
         @test FHIRClient.BaseURL(uri; require_https = false) isa FHIRClient.BaseURL
     end
 
     http_uris = ["http://example.com", HTTP.URI("http://example.com")]
-    @testset for uri in uris
+    @testset for uri in http_uris
         @test_throws Exception FHIRClient.BaseURL(uri)
         @test_throws Exception FHIRClient.BaseURL(uri; require_https = true)
         @test FHIRClient.BaseURL(uri; require_https = false) isa FHIRClient.BaseURL
