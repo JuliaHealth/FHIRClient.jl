c364a95f510ff52db2009cefd56846e4bd400764
Dilum Aluthge <dilum@aluthge.com>
2023-02-27 20:23:10 -0500

    Fix some mistakes

--- test/unit/types.jl
+++ test/unit/types.jl
@@ -1,16 +1,16 @@
 @testset "BaseURL constructor"
     https_uris = ["https://example.com", HTTP.URI("https://example.com")]
     @testset for uri in uris
-        @test_throws FHIRClient.BaseURL(HTTP.URI("http://example.com")) isa FHIRClient.BaseURL
-        @test_throws FHIRClient.BaseURL(HTTP.URI("http://example.com"); require_https = true) isa FHIRClient.BaseURL
-        @test_throws FHIRClient.BaseURL(HTTP.URI("http://example.com"); require_https = false) isa FHIRClient.BaseURL
+        @test FHIRClient.BaseURL(uri) isa FHIRClient.BaseURL
+        @test FHIRClient.BaseURL(uri; require_https = true) isa FHIRClient.BaseURL
+        @test FHIRClient.BaseURL(uri; require_https = false) isa FHIRClient.BaseURL
     end
 
     http_uris = ["http://example.com", HTTP.URI("http://example.com")]
     @testset for uri in uris
-        @test_throws Exception FHIRClient.BaseURL(HTTP.URI("http://example.com"))
-        @test_throws Exception FHIRClient.BaseURL(HTTP.URI("http://example.com"); require_https = true)
-        @test FHIRClient.BaseURL(HTTP.URI("http://example.com"); require_https = false) isa FHIRClient.BaseURL
-        @test_logs (:warn,) match_mode=:any FHIRClient.BaseURL(HTTP.URI("http://example.com"); require_https = false)
+        @test_throws Exception FHIRClient.BaseURL(uri)
+        @test_throws Exception FHIRClient.BaseURL(uri; require_https = true)
+        @test FHIRClient.BaseURL(uri; require_https = false) isa FHIRClient.BaseURL
+        @test_logs (:warn,) match_mode=:any FHIRClient.BaseURL(uri; require_https = false)
     end
 end
