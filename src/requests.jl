a3362d6c52504d7ed9a99cf79f9710340fecbd93
David Widmann <devmotion@users.noreply.github.com>
2023-11-09 16:31:02 +0100

    Check scheme, host, and path of the request

--- src/requests.jl
+++ src/requests.jl
@@ -62,14 +62,14 @@ end
     return HTTP.URI(string(_url_string, "/"))
 end
 
-@inline function _generate_full_url(client::Client, path::AbstractString)::HTTP.URI
+@inline function _generate_full_url(base::HTTP.URI, path::AbstractString)::HTTP.URI
     # Add trailing slash to base URL
     # This is important since
     # URIs.resolvereference("http://foo/bar", "./baz") = URIs.URI("http://foo/baz")
     # whereas
     # URIs.resolvereference("http://foo/bar/", "./baz") = URIs.URI("http://foo/bar/baz")
     # (compliant with RFC 3986 Section 5.2)
-    base_url = _add_trailing_slash(_get_http_uri(get_base_url(client)))
+    base_url = _add_trailing_slash(_get_http_uri(base))
 
     # Treat all paths without scheme as relative
     # Adding the dot is important since
@@ -83,18 +83,38 @@ end
 end
 
 """
-    request_raw(client, verb, path; query, body, headers)
-    request_raw(client, verb, path; query, headers)
-    request_raw(client, verb, path; body, headers)
-    request_raw(client, verb, path; headers)
+    request_raw(client::Client, verb::AbstractString, path::AbstractString; kwargs...)
+
+Perform a request with target `path` and method `verb` (such as `"GET"` or `"POST"`)
+for the FHIR `client`, and return a `String` of the body of the response.
+
+# Keyword arguments
+
+- `body::Union{AbstractString, Nothing}` (default: `nothing`): Body of the request.
+- `headers::AbstractDict` (default: `Dict{String, String}()`): Headers of the request.
+- `query::Union{AbstractDict, Nothing}` (default: `nothing`): Query parameters.
+- `require_client_scheme::Bool` (default: `true`): Whether the scheme (`http`/`https`) of the requested URL has to be the scheme of the FHIR `client`.
+- `require_client_host::Bool` (default: `true`): Whether the host of the requested URL has to be the host of the FHIR `client`.
+- `require_client_path::Bool` (default: `true`): Whether the path of the requested URL has to be a subpath of the path of the FHIR `client`.
 """
 @inline function request_raw(client::Client,
                              verb::AbstractString,
                              path::AbstractString;
                              body::Union{AbstractString, Nothing} = nothing,
                              headers::AbstractDict = Dict{String, String}(),
-                             query::Union{AbstractDict, Nothing} = nothing)::String
-    response = _request_raw_response(client, verb, path; body=body, headers=headers, query=query)
+                             query::Union{AbstractDict, Nothing} = nothing,
+                             require_client_scheme::Bool = true,
+                             require_client_host::Bool = true,
+                             require_client_path::Bool = true)::String
+    response = _request_raw_response(client,
+                                     verb,
+                                     path;
+                                     body = body,
+                                     headers = headers,
+                                     query = query,
+                                     require_client_scheme = require_client_scheme,
+                                     require_client_host = require_client_host,
+                                     require_client_path = require_client_path)
     response_body_string::String = String(response.body)::String
     return response_body_string
 end
@@ -104,9 +124,23 @@ end
                              path::AbstractString;
                              body::Union{AbstractString, Nothing} = nothing,
                              headers::AbstractDict = Dict{String, String}(),
-                             query::Union{AbstractDict, Nothing} = nothing)
-    full_url = _generate_full_url(client,
-                                  path)
+                             query::Union{AbstractDict, Nothing} = nothing,
+                             require_client_scheme::Bool = true,
+                             require_client_host::Bool = true,
+                             require_client_path::Bool = true)
+    # Construct and check the validity of the target URL
+    base_url = get_base_url(client)
+    full_url = _generate_full_url(base_url, path)
+    if require_client_scheme && full_url.scheme != base_url.scheme
+        throw(ArgumentError("The requested URL ($full_url) must have the same scheme as the FHIR client ($base_url)."))
+    end
+    if require_client_host && full_url.host != base_url.host
+        throw(ArgumentError("The requested URL ($full_url) must have the same host as the FHIR client ($base_url)."))
+    end
+    if require_client_path && !startswith(full_url.path, base_url.path)
+        throw(ArgumentError("The requested URL ($full_url) must be a subpath of the FHIR client ($base_url)."))
+    end
+
     _new_headers = Dict{String, String}()
     json_headers!(_new_headers)
     authentication_headers!(_new_headers, client)
@@ -130,24 +164,27 @@ end
 end
 
 """
-    request_json(client, verb, path; query, body, headers)
-    request_json(client, verb, path; query, headers)
-    request_json(client, verb, path; body, headers)
-    request_json(client, verb, path; headers)
+    request_json(client::Client, verb::AbstractString, path; query, body, headers)
 """
 @inline function request_json(client::Client,
                               verb::AbstractString,
                               path::AbstractString;
                               body::Union{JSON3.Object, Nothing} = nothing,
                               headers::AbstractDict = Dict{String, String}(),
-                              query::Union{AbstractDict, Nothing} = nothing)
+                              query::Union{AbstractDict, Nothing} = nothing,
+                              require_client_scheme::Bool = true,
+                              require_client_host::Bool = true,
+                              require_client_path::Bool = true)
     _new_request_body = _write_json_request_body(body)
     response_body::String = request_raw(client,
                                         verb,
                                         path;
                                         body = _new_request_body,
                                         headers = headers,
-                                        query = query)::String
+                                        query = query,
+                                        require_client_scheme = require_client_scheme,
+                                        require_client_host = require_client_host,
+                                        require_client_path = require_client_path)::String
     response_json = JSON3.read(response_body)
     return response_json
 end
@@ -174,6 +211,9 @@ end
                          body = nothing,
                          headers::AbstractDict = Dict{String, String}(),
                          query::Union{AbstractDict, Nothing} = nothing,
+                         require_client_scheme::Bool = true,
+                         require_client_host::Bool = true,
+                         require_client_path::Bool = true,
                          kwargs...)::T where T
     _new_request_body = _write_struct_request_body(body)
     response_body::String = request_raw(client,
@@ -181,7 +221,10 @@ end
                                         path;
                                         body = _new_request_body,
                                         headers = headers,
-                                        query = query)::String
+                                        query = query
+                                        require_client_scheme = require_client_scheme,
+                                        require_client_host = require_client_host,
+                                        require_client_path = require_client_path)::String
     response_object::T = JSON3.read(response_body,
                                     T;
                                     kwargs...)::T
