e179740c4981826951fa90e37185cd7281804d0c
Dilum Aluthge <dilum@aluthge.com>
2023-12-03 10:43:17 -0500

    Log the parsed (and pretty-printed) response body at `LogLevel(-3_000`)

---
+++ src/tryparse.jl
@@ -0,0 +1,15 @@
+function tryparse_json(response_body::AbstractString)
+    response_json = try
+        JSON3.read(response_body)
+    catch ex
+        # If no exception is thrown, we don't need to log `response_body`.
+        #
+        # However, if an exception is thrown, we need to log the entire raw
+        # `response_body` so that the user can try to figure out why it failed to
+        # parse as valid JSON.
+        bt = catch_backtrace()
+        @logmsg LogLevel(-3_000) "tryparse_json()" exception=(ex,bt) response_body
+        nothing
+    end
+    return response_json
+end
